07d623a2211dfc6839dac6f206bba0f9576db6b6
donald-shenaj <donald.shenaj@studenti.unipd.it>
2020-06-25 21:59:38 +0200

    Improved formatting

--- src/video-classifier.py
+++ src/video-classifier.py
@@ -5,6 +5,7 @@ import argparse
 from vidgear.gears import CamGear
 
 
+# switcher used to get the corresponding youtube video url from the given input name
 def video_url(argument):
     switcher = {   
         "adam" : lambda : "https://www.youtube.com/watch?v=eghK5yMpNuc",
@@ -16,30 +17,33 @@ def video_url(argument):
 
 def main():
     
+    # extract the video url
     parser = argparse.ArgumentParser(description='Actor video classifier')
     parser.add_argument('--input', type=str, help='Path to video')  
     args = parser.parse_args()
-    
     url_video = video_url(args.input)
     
-    classes = ["Adam Sandler", "Alyssa Milano", "Bruce Willis", "Denise Richards",
-            "George Clooney", "Gwyneth Paltrow", "Hugh Jackman", "Jason Statham",
-            "Jennifer Love Hewitt", "Lindsay Lohan", "Mark Ruffalo", 
-            "Robert Downey Jr", "Will Smith"]
+    # load the classes used in our CNN
+    classes = [ "Adam Sandler", "Alyssa Milano", "Bruce Willis", "Denise Richards",
+                "George Clooney", "Gwyneth Paltrow", "Hugh Jackman", "Jason Statham",
+                "Jennifer Love Hewitt", "Lindsay Lohan", "Mark Ruffalo", 
+                "Robert Downey Jr", "Will Smith" ]
 
 
+    # load the trained CascadeClassifier for face detection
     face_cascade = cv2.CascadeClassifier('../data/face_cascade_cv2/haarcascade_frontalface_default.xml')
+    
+    # load the CNN trained with matlab
     net = cv2.dnn.readNetFromONNX("../data/trained_net/CNNNet_13.onnx")
     
-
-
+    # load the youtube video
     stream = CamGear(source=url_video, y_tube=True).start() # YouTube Video URL as input
 
-    count = 0
+
     while True:
         frame_normal = stream.read()
         
-        if frame_normal is None:   #se sono finiti i frame del video
+        if frame_normal is None:   
             print("End of the video")
             break
         
@@ -48,7 +52,7 @@ def main():
 
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         
-        blobs = []
+        
         faces = face_cascade.detectMultiScale(gray, 1.3, 5)
         for (x,y,w,h) in faces:
             frame = cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
@@ -62,6 +66,8 @@ def main():
             end = time.time()
             print()
             print("[INFO] classification took {:.5} seconds".format(end - start))
+            
+            
             # sort the indexes of the probabilities in descending order (higher
             # probabilitiy first) and grab the top-5 predictions
             idxs = np.argsort(preds[0])[::-1][:5]
@@ -69,14 +75,14 @@ def main():
             for (i, idx) in enumerate(idxs):
                 # draw the top prediction on the input image
                 if i == 0:
-                    text = "Label: {}, {:.2f}%".format(classes[idx],
-                        preds[0][idx] * 100)
+                    text = "Label: {}, {:.2f}%".format(classes[idx], preds[0][idx] * 100)
+                    
                     cv2.putText(frame, text, (5, 25),  cv2.FONT_HERSHEY_SIMPLEX,
                         0.7, (0, 0, 255), 2)
+                    
                 # display the predicted label + associated probability to the
                 # console	
-                print("[INFO] {}. label: {}, probability: {:.5}".format(i + 1,
-                    classes[idx], preds[0][idx]))
+                print("[INFO] {}. label: {}, probability: {:.5}".format(i + 1, classes[idx], preds[0][idx]))
                             
         cv2.imshow('videoframe',frame)    
         
